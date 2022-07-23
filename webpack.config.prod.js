b2aaa17ef998d6f7bf000dce7f50ccc6e2623d3f
olegweremyew <olegweremey1994@mail.ru>
2022-07-23 22:24:22 +0300

    optimization

---
+++ webpack.config.prod.js
@@ -0,0 +1,50 @@
+const CompressionPlugin = require('compression-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: ['./public/src/index.js'],
+  output: {
+    path: `${__dirname}/public/`,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel',
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      // <--key to reduce React's size
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new CompressionPlugin({
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+  ],
+
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    alias: {
+      react: 'preact-compat',
+      'react-dom': 'preact-compat',
+    },
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './public',
+  },
+};
