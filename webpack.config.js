0161a8770b50f3552ccb907344e8f57153d1e326
olegweremyew <olegweremey1994@mail.ru>
2022-07-23 23:55:14 +0300

    updated site performance

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  entry: [
+    './public/src/index.js'
+  ],
+  output: {
+    path: __dirname + '/public/',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devtool: "eval",
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    "alias": {
+      "react": "preact-compat",
+      "react-dom": "preact-compat"
+    }
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './public'
+  }
+};
